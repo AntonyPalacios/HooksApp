f2c939e02ef46ba67191468fcb69c89279c935cb
Antony Palacios Coronado <apalaciosc@pucp.edu.pe>
2024-12-09 17:02:11 -0500

    useLayoutEffect

--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -1,16 +1,35 @@
-export const PokemonCard = ({id,name,sprites=[]})=> {
+import {useLayoutEffect, useRef, useState} from "react";
+
+export const PokemonCard = ({id, name, sprites = []}) => {
+
+    const pRef = useRef();
+    const [boxSize, setBoxSize] = useState({
+        width: 0,
+        height: 0,
+    });
+    useLayoutEffect(() => {
+        const {height, width} = pRef.current.getBoundingClientRect();
+        setBoxSize({
+            width,
+            height
+        })
+    }, [name])
+
     return (
-        <section style={{height:'200'}}>
-            <h2 className="text-capitalize">#{id} - {name}</h2>
+        <>
+            <section style={{display: 'flex'}}>
+                <h2 ref={pRef} className="text-capitalize">#{id} - {name}</h2>
 
-            <div>
-                {
-                    sprites.map(sprite=> (
-                        <img key={sprite} src={sprite} alt={name} />
-                    ))
-                }
+                <div>
+                    {
+                        sprites.map(sprite => (
+                            <img key={sprite} src={sprite} alt={name}/>
+                        ))
+                    }
 
-            </div>
-        </section>
+                </div>
+            </section>
+            <code>{JSON.stringify(boxSize)}</code>
+        </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,11 @@ import {SimpleForm} from "./02-useEffect/SimpleForm.jsx";
 import {FormWithCustomHook} from "./02-useEffect/FormWithCustomHook.jsx";
 import {MultipleCustomHooks} from "./03-examples/MultipleCustomHooks.jsx";
 import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
+import {Layout} from "./05-useLayoutEffect/Layout.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <FocusScreen />
+    <Layout />
   // </StrictMode>
 )
