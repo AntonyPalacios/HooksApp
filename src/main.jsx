0f00e6e7c1693deac09b243dbe5cd5a9bc7fa15e
Antony Palacios Coronado <apalaciosc@pucp.edu.pe>
2024-12-09 17:25:09 -0500

    useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,12 @@ import {FormWithCustomHook} from "./02-useEffect/FormWithCustomHook.jsx";
 import {MultipleCustomHooks} from "./03-examples/MultipleCustomHooks.jsx";
 import {FocusScreen} from "./04-useRef/FocusScreen.jsx";
 import {Layout} from "./05-useLayoutEffect/Layout.jsx";
+import {Memorize} from "./06-memos/Memorize.jsx";
+import {MemoHook} from "./06-memos/MemoHook.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <Layout />
+    <MemoHook />
   // </StrictMode>
 )
