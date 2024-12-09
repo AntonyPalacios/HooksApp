6586a8967765aa905feb9852f61d0455ecef441e
Antony Palacios Coronado <apalaciosc@pucp.edu.pe>
2024-12-09 17:50:47 -0500

    tarea

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,11 @@ import {Layout} from "./05-useLayoutEffect/Layout.jsx";
 import {Memorize} from "./06-memos/Memorize.jsx";
 import {MemoHook} from "./06-memos/MemoHook.jsx";
 import {CallbackHook} from "./06-memos/CallbackHook.jsx";
+import {Padre} from "./07-tarea-memo/Padre.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <CallbackHook />
+    <Padre />
   // </StrictMode>
 )
