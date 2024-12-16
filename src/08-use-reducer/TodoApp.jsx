26beed81be8d58fc7eb8d77ef2b6fb485bf8cb98
Antony <apalaciosc0109@gmail.com>
2024-12-16 17:54:11 -0500

    useTodos custom hook

--- src/08-use-reducer/TodoApp.jsx
+++ src/08-use-reducer/TodoApp.jsx
@@ -1,50 +1,12 @@
-import {useEffect, useReducer} from "react";
-import {todoReducer} from "./todoReducer.js";
+import {useEffect} from "react";
 import {TodoList} from "./TodoList.jsx";
 import {TodoAdd} from "./TodoAdd.jsx";
+import {useTodo} from "../hooks/useTodo.js";
 
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Aprender React',
-    //     done: false
-    // }
-]
-const init = ()=>{
-    return JSON.parse(localStorage.getItem('todos')) || []
-}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
-
-    useEffect(()=>{
-        localStorage.setItem('todos',JSON.stringify(todos))
-    },[todos])
-
-    const handleNewTodo = (todo) =>{
-        const action = {
-            type:'[TODO] Add Todo',
-            payload:todo
-        }
-        dispatch(action)
-    }
-
-    const handleDeleteTodo = (id) =>{
-        const action = {
-            type:'[TODO] Delete Todo',
-            payload:id
-        }
-        dispatch(action)
-
-    }
-    const handleToggleTodo = (id) =>{
-        const action = {
-            type:'[TODO] Toggle Todo',
-            payload:id
-        }
-        dispatch(action)
-    }
+    const {todos,handleDeleteTodo,handleNewTodo,handleToggleTodo} = useTodo()
 
     return (
         <>
